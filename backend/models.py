54ac70c4baaf60dd188351512d596d3d9dcb3aec
marco <luchianmarco22@gmail.com>
2025-09-11 17:37:33 +0100

    Fix User model: hash password on creation to prevent CI signup errors

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,4 @@
-# models.py
-from extensions import db  # nu mai importăm din app.py
+from extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
@@ -7,11 +6,16 @@ class User(db.Model):
     username = db.Column(db.String(64), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
 
+    def __init__(self, username, password):
+        self.username = username
+        self.set_password(password)
+
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,5 +23,3 @@ class Task(db.Model):
     status = db.Column(db.String(20), nullable=False, default='To do')
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('tasks', lazy=True))
-
-
